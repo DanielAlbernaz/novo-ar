faa9cc891a773f4a8a868814698342f0e1de0e83
Ruthtlidia <76877776+Ruthtlidia@users.noreply.github.com>
2021-02-26 22:38:34 -0300

    Menu principal
    menu

--- resources/views/site/main/header.blade.php
+++ resources/views/site/main/header.blade.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="UTF-8"><title>Novo Ar Climatizadores</title>
+        <meta name="description" content="Agência  especializada em Marketing Digital, Criação de Sites e Splicativos Mobile ">
+        <meta name="keywords" content="Agência Digital, Marketing, Sites">
+        <meta name="robots" content="index, follow">
+        <meta name="author" content="Ruth Lídia">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+        <link rel="icon" href="./assests/images/icon.png">
+   		 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
 
     <link href="{{asset('assests/site/css/style.css')}}" rel="stylesheet" type="text/css">
     
