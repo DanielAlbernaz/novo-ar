ede9434533f6612b08249fcbe03746ee6d894fa7
Ruthtlidia <76877776+Ruthtlidia@users.noreply.github.com>
2021-03-24 10:04:16 -0300

    Ficha tecnica concluida

--- resources/views/site/paginas/produto.blade.php
+++ resources/views/site/paginas/produto.blade.php
@@ -26,6 +26,45 @@
                     </div>
                 </div>
              </div>
+             <div class="row-descricao">
+                <div class="ficha">
+                    <div class="text-ficha">
+                        <div class="titulo-text">
+                            <h2>Ficha Técnica</h2>
+                        </div>
+                        <div class="valor-text">
+                            <p>
+                                <strong> Vazão de Ar: </strong>
+                            </p>
+                            <p>18.00m³/h</p>
+                        </div>
+                        <div class="valor-text">
+                            <p>
+                                <strong> Motor: </strong>
+                            </p>
+                            <p>1cv 220v</p>
+                        </div>
+                        <div class="valor-text">
+                            <p>
+                                <strong> Consumo elétrico: </strong>
+                            </p>
+                            <p>8,84 KW/h</p>
+                        </div>
+                        <div class="valor-text">
+                            <p>
+                                <strong> Abertura de parede: </strong>
+                            </p>
+                            <p>82X82cm</p>
+                        </div>
+                        <div class="valor-text">
+                            <p>
+                                <strong> Reservatório / peso seco: </strong>
+                            </p>
+                            <p>18L / 89kg</p>
+                        </div>
+                    </div>
+                </div>
+             </div>
         </div>
     </div>    
 @include('site.main.footer')
