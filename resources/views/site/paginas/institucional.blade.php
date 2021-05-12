0beca154999d27656cf555d6c5b1cfb68d9c7ecc
Ruthtlidia <ruthtlidia@gmail.com>
2021-05-12 17:29:21 -0300

    Atualização
    Teste

--- resources/views/site/paginas/institucional.blade.php
+++ resources/views/site/paginas/institucional.blade.php
@@ -1,77 +1,116 @@
 @include('site.main.header')
 
-<div class="titprincipal">
-    <h1> SOBRE A EMPRESA</h1>
-</div>
+	<div class="migalhaMBI imgMigalha" style="background-size: cover; background-image: url('images/home.jpg');">
+		<h1>QUEM SOMOS</h1>
+	</div>
 
-<div class="folder">
-    <div class="fol-fol">
+		<!-- About page one start here -->
+		<div class="about-page-one-area">
+			<div class="contai">
+				<!-- video institucional -->
+				
 
-        <div class="contaain">
+				<h3>
+					Tradição e qualidade desde 1999 o Grupo Lion está fixado no mercado com segmento fitness sendo graduada hoje a uma das maiores empresas nacionais no segmento. <br>
+					
+					Diante da necessidade cada vez maior das academias de climatizar seu espaço com eficiência e oferecer bem-estar aos seus alunos, a empresa começa a estudar os melhores produtos internacionais para oferecer uma opção melhor e mais viável ao ar condicionado mantendo o custo baixo operacional. <br>
+					
+					Contando com a alta capacidade de processo produtivo nasce então a marca Lion Clima. É então que começa a fabricar climatizadores de ambiente que surge como opção mais econômica e eficiente ao ar condicionado e abrangendo não só academias, mas também qualquer tipo de ambiente seja ele industrial, comercial, festas ou agrícolas. <br>
+					
+					Hoje a Lion atua com 2 unidades, uma em Valentim Gentil localizada no interior de São Paulo, e a outra em Paranaíba em Mato Grosso do Sul, gerando mais de 200 empregos diretos, fortalecendo a economia das respectivas regiões.
+					
+				</h3>
+			
+				<!--/fim video institucional -->
+				
+				
+				<div class="row row-flex">
+					
+					<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+						<div class="about-content">
+							<h2 class="title-bar">Novo Ar </h2>
+							<p class="style-bar">
+								Há anos promovendo o bem estar a todos.                        </p>
+							<p class="style-bar">						
+								<strong>R. 60, 25 -St. oeste, Goiânia - Go, 74.253-115</strong><br/>
+								<a class="tel-bar" href="tel:556240088844"><strong>Vendas: (62) 3568-2569</strong></a><br/>
+								<a class="tel-bar" href="tel:556240088877"><strong>Aluguel: (62) 3654-8546</strong></a><br/>
 
-           <div class="fundo-banner">
-                
-            </div>
-
-            <div class="titulo-fol">
-                <h1>A Novo Ar</h1>
-            </div>
-
-            <div class="texto-about">
-                <h2>
-                    <p>Tradição e qualidade desde 1999 o Grupo Lion está fixado no mercado com segmento fitness sendo graduada hoje a uma das maiores empresas nacionais no segmento. </p>
-
-                    <p>Diante da necessidade cada vez maior das academias de climatizar seu espaço com eficiência e oferecer bem-estar aos seus alunos, a empresa começa a estudar os melhores produtos internacionais para oferecer uma opção melhor e mais viável ao ar condicionado mantendo o custo baixo operacional. </p>
-                    
-                    <p>Contando com a alta capacidade de processo produtivo nasce então a marca Lion Clima. É então que começa a fabricar climatizadores de ambiente que surge como opção mais econômica e eficiente ao ar condicionado e abrangendo não só academias, mas também qualquer tipo de ambiente seja ele industrial, comercial, festas ou agrícolas. </p>
-                    
-                    <p>Hoje a Lion atua com 2 unidades, uma em Valentim Gentil localizada no interior de São Paulo, e a outra em Paranaíba em Mato Grosso do Sul, gerando mais de 200 empregos diretos, fortalecendo a economia das respectivas regiões.</p>
-                </h2>
-
-            </div>
-
-        </div>
-
-    </div>  
-</div>
-
-
-<div class="colunnas">
+														
+							</p>
+							
+						</div>
+					</div>
+	
+					<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">                    
+						<img class="img-responsive" src="images/product1.png" alt="video">                                           
+					</div>
+													<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div  style="border-bottom:#EFEFEF 1px solid; height: 1px; width: 97%; margin: auto; margin-top: 35px; margin-bottom: 35px;"></div>
+					</div>
+				</div>
+				
+			</div>
+    	</div>
+    <!-- About page one End here -->
     
-    <div class="collus">
+<div class="clearfix"></div>    
+    
+<div class="contai">
+	<div class="row">
+	   <!-- Mensagem -->
+		<div class="col-md-4">
+			<h4 class="headline margin-bottom-30">Sinta-se à vontade para falar conosco.</h4>			
+			<div class="sidebar-textbox">
+				<p style="text-align: justify; font-style: normal;">Utilize este espaço para falar conosco sobre suas dúvidas, suas recomendações, instruções de trabalho ou sobre qualquer outro assunto.<br />
+Críticas construtivas são muito bem vindas. Elas receberão toda a nossa atenção e fazemos questão de lhe dar um retorno sobre elas. Por isso precisamos que você se identifique e nos informe como prefere receber relatos sobre nossas providências.</p>				
+			</div>
+		</div>
+		<!--/fim Mensagem -->
+		<!-- Formulário -->
+		<div class="col-md-8">
+			<section id="contact">
+				<h4 class="headline margin-bottom-35">Fale Conosco</h4>
+				<div id="contact-message"></div> 
+				<form method="post" action="javascript:;" name="frmContato" id="frmContato" autocomplete="on">
+					<!-- nome -->
+					<div>
+						<input class="inputLogin validate[required]" name="name" type="text" id="name" placeholder="Nome" required="required" />
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<!-- email -->
+							<div>
+								<input name="email" type="email" id="email" placeholder="E-mail" required="required" class="inputLogin validate[required,custom[email]]" />
+							</div>
+						</div>
+						<div class="col-md-6">
+        					<!-- Telefone -->
+        					<div>
+        						<input class="inputLogin sp_celphones" name="telefone" type="text" id="telefone" placeholder="Telefone" required="required" />
+        					</div>
+						</div>
+					</div>
+					<!-- Mensagem -->
+					<div>
+						<textarea class="inputLogin validate[required]" name="mensagem" cols="40" rows="3" id="mensagem" placeholder="Mensagem" spellcheck="true" required="required"></textarea>
+					</div>
 
-        <div class="img-princi">
-            <img src="./images/bannerhero.png" alt="">
-        </div>
+					<label>
+                        <span class="text-input">
+                            <div class="g-recaptcha" data-sitekey="6Lc01FAUAAAAABKdj75TTulyB5c8lTOx-X947lmC"></div><br/>
+                        </span>
+                    </label>
+					<input type="submit" class="submit button" id="submit" value="ENVIAR" />
+				</form>
+			</section>
+		</div>
+		<!--/fim Formulário -->
+	</div>
+</div>    
+<div class="margin-bottom-40"></div>       <!--/FIM INTERNAS  --> 
 
-        <div class="princ-titulo">
-            <h2> Nossos Príncipios</h2>
-            <p>Responsabilidade para cuidar do bem estar dos nossos clientes.</p>
-        </div>
 
-        <div class="colls">
-
-            <div class="colls-1">
-                <article>
-                    
-                </article>
-            </div>
-
-            <div class="colls-1">
-                <article>
-                    
-                </article>
-            </div>
-
-            <div class="colls-1">
-                <article>
-                    
-                </article>
-            </div>
-            
-        </div>
-    </div>
-</div>
 
 
 @include('site.main.footer')
---
+++ resources/views/site/paginas/privacidade.blade.php
@@ -0,0 +1,41 @@
+@include('site.main.header')
+
+<div class="migalhaMBI imgMigalha" style="background-size: cover; background-image: url('img/home-roof-1440x375.jpg');">
+	<h1>POLÍTICA DE PRIVACIDADE</h1>
+
+</div>
+    <!-- About page one start here -->
+    <div class="about-page-one-area">
+        <div class="container">
+
+            
+            
+            <div class="row">
+
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="about-content">
+                        <h2 class="title-bar">POLÍTICA DE PRIVACIDADE MBI</h2>
+                        <p style="text-align: justify;">
+                        Todas as informações pessoais, relativas a clientes ou visitantes, entregues a MBI Imóveis serão tratadas em concordância com a Lei Geral de Proteção de Dados Pessoais (Lei n&ordm; 13.709/2018).<br />
+<br />
+A informação pessoal recolhida pode incluir o seu nome, e-mail e número de telefone.<br />
+<br />
+O presente termo permite que a MBI Imóveis use suas informações pessoais para diferentes finalidades. O e-mail e o telefone são utilizados para que um de nossos especialistas entre em contato e forneça o atendimento desejado sob o anúncio em questão. O e-mail também pode ser usado para envio de ofertas de imóveis de acordo com o perfil do cliente perante as características do imóvel.<br />
+<br />
+As Informações confidenciais, obtidas por qualquer meio, deverão ser tratadas como confidenciais e mantidas em sigilo, limitando-se sua divulgação e acesso aos parceiros diretamente envolvidos, e compartilhadas exclusivamente no ambiente de trabalho. A confidencialidade das informações deverá ser preservada mesmo após o encerramento do vínculo com a MBI Imóveis.<br />
+<br />
+A MBI Imóveis também se compromete a não vender, alugar ou repassar suas informações para terceiros. A única exceção está em casos em que essas informações forem exigidas judicialmente.<br />
+<br />
+Ao nos enviar estes dados entendemos que você aceita este acordo de privacidade. A MBI Imóveis reserva-se ao direito de alterar este acordo sem aviso prévio. Deste modo, recomendamos que consulte a nossa política de privacidade com regularidade de forma a estar sempre atualizado.                        </p>                                                
+                    </div>
+                </div>
+
+            	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            		<div  style="border-bottom:#EFEFEF 1px solid; height: 1px; width: 97%; margin: auto; margin-top: 35px; margin-bottom: 35px;"></div>
+            	</div>
+            </div>
+        </div>
+    </div>
+
+
+@include('site.main.footer')
